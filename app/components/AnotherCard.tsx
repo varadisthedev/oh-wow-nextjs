009e97804aeb80ce80e6a01646527778988f8a95
varadisthedev <varadisthedev@gmail.com>
2025-12-15 23:52:07 +0530

    redeisgn: changed card component's deisgn

--- app/components/AnotherCard.tsx
+++ app/components/AnotherCard.tsx
@@ -9,7 +9,7 @@ export const AnotherCard = ({amount,title}:Props) => {
     //computing class beforehand
     const classAmountColor= amount>LIMIT? "text-red-500" : "text-green-500";
   return (
-    <article className='bg-[hsl(0,0,10)] border-4 border-[hsl(0,10,5)] rounded-3xl w-80 h-60 m-3 p-8'> 
+    <article className='bg-[hsl(0,0,10)] border-4 border-[hsl(0,10,5)] rounded-3xl w-80 h-40 m-3 p-8'> 
         <h1 className='text-2xl font-bold'>{title}</h1>
         <span className={`${classAmountColor} text-2xl font-bold`}>₹{amount}</span>
     </article>
