15604353f35b5c8dd94b59cfdd716a8310b53c97
varadisthedev <varadisthedev@gmail.com>
2026-01-04 23:26:31 +0530

    add: fetching user names from backend

---
+++ app/user/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { useEffect, useState } from "react"
+export default function page() {
+    const [users, setUsers] = useState<string[]>([]);
+
+    const FetchUsers = async () => {
+        const response = await fetch("/api/users");
+        const data = await response.json();
+        console.log("data fetched from /api/users");
+        setUsers(data.users);
+    } // users: ["Varad","sharvari","adt"]
+
+    useEffect(() => {
+        FetchUsers();
+    }, []);
+    // will run when the component mounts
+
+    return (
+        <>
+            {/* list of user names with numbers */}
+            <div>
+                <ul>
+                    {users.map((content, i) => (
+                        <li key={i}>{i + 1}. {content}</li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    )
+}
