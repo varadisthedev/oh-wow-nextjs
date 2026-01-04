2be754c7bec8453aa772f778b90d6f3d33640a13
varadisthedev <varadisthedev@gmail.com>
2026-01-04 23:25:52 +0530

    add: working with next router

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+export default function page() {
+  return (
+    <div>
+        <h1>this is a dashboard</h1>
+    </div>
+  )
+}
---
+++ app/landing_page/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <h1 className='font-extrabold text-7xl bg-blue-950 align-middle'>welcome to <br></br>ABYSS</h1>
+    </div>
+  )
+}
